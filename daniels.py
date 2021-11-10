14f79ef9737be5e78d02e2f3ede9e4898eb11287
ovxon <dclasher22@gmail.com>
2021-11-10 13:50:39 +0200

    Pievienoju 1. malas ievadi

--- daniels.py
+++ daniels.py
@@ -1,1 +1,12 @@
 # Veidoju programmu ar kuru aprēķināt trijstūra laukumu zinot tā malas
+
+while True:
+            a = input("Ievadiet 1. malas vērtību: ")
+            try:
+                a = float(a)
+                if a <= 0:
+                    print("Mala nevar būt 0")
+                else:
+                    break
+            except ValueError:
+                print("Ievadītā vērtība nav skaitlis, mēģiniet vēlreiz")
