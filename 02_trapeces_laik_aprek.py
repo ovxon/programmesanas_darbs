52016e71337802d7e218939e54ec8d4e4a2bdf3d
sofijadolga <93522523+sofijadolga@users.noreply.github.com>
2021-11-11 21:20:53 +0200

    ievadiju trapeces laukuma formulu un izvadiju

--- 02_trapeces_laik_aprek.py
+++
@@ -1,3 +0,0 @@
-"""
-Trapces laukuma aprēķināšana. 
-"""
--- 02_trapeces_lauk_aprek.py
+++ 02_trapeces_lauk_aprek.py
@@ -6,3 +6,5 @@ a = float(input("1. pamata garums:"))
 b = float(input("2. pamata garums: "))
 h = float(input("Trapeces augstums:"))
 
+laukums = ((a + b) / 2) * h
+print("Trapeces laukums ir:", laukums)
