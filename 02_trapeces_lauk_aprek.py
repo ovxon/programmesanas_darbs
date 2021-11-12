093649ef0411e63ed6c33338846b607499f6c257
ovxon <dclasher22@gmail.com>
2021-11-12 10:28:57 +0200

    uzlaboju formatēšanu 10.rindā

--- 02_trapeces_lauk_aprek.py
+++ 02_trapeces_lauk_aprek.py
@@ -1,10 +1,10 @@
 """
-Trapces laukuma aprēķināšana. 
+Trapeces laukuma aprēķināšana. 
 """
 
-a = float(input("1. pamata garums:"))
+a = float(input("1. pamata garums: "))
 b = float(input("2. pamata garums: "))
-h = float(input("Trapeces augstums:"))
+h = float(input("Trapeces augstums: "))
 
 laukums = ((a + b) / 2) * h
-print("Trapeces laukums ir:", laukums)
+print(f"Trapeces laukums ir: {laukums}")
