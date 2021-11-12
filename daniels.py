d2802194caa3e4d6a56ba2c5526b0e5fd00531b5
ovxon <dclasher22@gmail.com>
2021-11-12 09:47:17 +0200

    pievienoju pārējos ievades logus

--- daniels.py
+++ daniels.py
@@ -1,7 +1,7 @@
 # Veidoju programmu ar kuru aprēķināt trijstūra laukumu zinot tā malas
 
 while True:
-            a = input("Ievadiet 1. malas vērtību: ")
+            a = input("Ievadiet 1. malas garumu: ")
             try:
                 a = float(a)
                 if a <= 0:
@@ -10,3 +10,26 @@ while True:
                     break
             except ValueError:
                 print("Ievadītā vērtība nav skaitlis, mēģiniet vēlreiz")
+
+while True:
+            b = input("Ievadiet 2. malas garumu: ")
+            try:
+                b = float(a)
+                if b <= 0:
+                    print("Mala nevar būt 0")
+                else:
+                    break
+            except ValueError:
+                print("Ievadītā vērtība nav skaitlis, mēģiniet vēlreiz")
+
+while True:
+            c = input("Ievadiet 3. malas garumu: ")
+            try:
+                c = float(c)
+                if c <= 0:
+                    print("Mala nevar būt 0")
+                else:
+                    break
+            except ValueError:
+                print("Ievadītā vērtība nav skaitlis, mēģiniet vēlreiz")
+
